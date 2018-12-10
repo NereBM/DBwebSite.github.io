4abf594a8d6dc8d099921308d2f828f53765890e
NereBM <44468540+NereBM@users.noreply.github.com>
2018-12-10 16:24:40 +0000

    last commit

--- MyModules2_detailsG54PRG.php
+++ MyModules2_detailsG54PRG.php
@@ -50,10 +50,10 @@
 		<div class="collapse navbar-collapse" id="navbarSupportedContent">
 			<ul class="navbar-nav mr-auto">
 				<li class="nav-item">
-					<a class="nav-link" href="#">Home </a>
+					<a class="nav-link" href="Home.html">Home </a>
 				</li>
 				<li class="nav-item active">
-					<a class="nav-link" href="#">My Modules <span class="sr-only">(current)</span> </a>
+					<a class="nav-link" href="MyModules2.php">My Modules <span class="sr-only">(current)</span> </a>
 				</li>
 				<li class="nav-item">
 					<a class="nav-link" href="#">My Timetable</a>
